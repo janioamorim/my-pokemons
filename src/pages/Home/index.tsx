13a9eda78f76b8363e40a94a0d1216f8220ad23d
Janio Amorim <janioamorim04@gmail.com>
2021-08-28 10:33:00 -0300

    Configuracoes iniciais, assets e page home

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+import { Container } from './styles';
+
+
+const Home: React.FC = () => {  
+
+  return (
+    <Container>
+      <p>Home</p>      
+    </Container>
+  );
+};
+
+export default Home;
